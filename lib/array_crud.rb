6f8539ef945c94657a5f56b43877a809ea3a4b05
careful-principle-0303 <cabaker96@aol.com>
2019-12-03 19:49:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,17 +1,17 @@
 def create_an_empty_array
-  
+  arr = []
 end
 
 def create_an_array
-  
+  arr = ["eggs", "beans"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array.push("peas")
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array.
 end
 
 def remove_element_from_end_of_array(array)
